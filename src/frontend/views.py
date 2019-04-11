cbf873dc953a0d3d4c3e4e189d280aafb7687110
ZadoyanMK <konzamir@gmail.com>
2019-04-11 22:09:21 +0300

    update confs to entrypoint.sh

--- src/frontend/views.py
+++ src/frontend/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 
 
-def index(request, pk=None, **kwargs):
+def index(request, pk=None, *a, **kwargs):
     return render(request, 'frontend/index.html')
 
 
-def redirect_to_index(request, **kwargs):
+def redirect_to_index(request, *a, **kwargs):
     return redirect('/en/')
 
 
-def redirect_to_404(request, **kw):
+def redirect_to_404(request, *a, **kw):
     return HttpResponseRedirect('/404/')
